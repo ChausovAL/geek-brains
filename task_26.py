ff2af305bf86bcad399f66abe038b4f016940454
ChausovAL <102621479+ChausovAL@users.noreply.github.com>
2023-06-28 07:56:25 +0300

    Add files via upload

---
+++ task_26.py
@@ -0,0 +1,6 @@
+def rec():
+    a = int(input("Введите число А: "))
+    b = int(input("Введите число B: "))
+    return a ** b
+
+print(rec())
---
+++ task_28.py
@@ -0,0 +1,7 @@
+def sum(a, b):
+    return a + b
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе чмсло: "))
+
+print(sum(a, b))
